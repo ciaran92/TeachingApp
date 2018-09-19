276d968ff3137567508f79f2ec94ad27758dbcfb
Nick McBride <nickumcb@gmail.com>
2018-09-19 22:14:10 +1200

    refactoring quiz.service.ts

--- TeachingAppAPI/Client-App/src/app/quiz/quiz.service.ts
+++ TeachingAppAPI/Client-App/src/app/quiz/quiz.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Question } from './question';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class QuizService {
     private questionsUrl = "http://localhost:52459/api/questions";
     private rootURL = "http://localhost:52459/api/users";
-    constructor(private http: Http){
-        
-    }
 
-    getQuestions(): Observable<Question[]>{
-        return this.http.get(this.questionsUrl).pipe(map((response: Response) => <Question[]>response.json()));
+    constructor(private http: HttpClient){}
+
+    getQuestions(): any {
+        var test = this.http.get(this.questionsUrl);
+        return test;
     }
 
     createUser(firstName: string, lastName: string, username: string, userPassword: string){
